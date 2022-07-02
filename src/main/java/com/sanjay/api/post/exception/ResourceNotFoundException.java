b2f8416b93b3e73e9c00a09cdd26f81d70880019
Sanjay Kumar S <s.sanjaykumar1@zohomail.in>
2022-07-03 00:17:57 +0530

    Post entity, repository and resourcenotfoundexception added

---
+++ src/main/java/com/sanjay/api/post/exception/ResourceNotFoundException.java
@@ -0,0 +1,36 @@
+package com.sanjay.api.post.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+/*
+@ResponseStatus indicates Spring-Boot to respond with defined status whenever this exception is thrown from our controller
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    private String resourceName;
+    private String fieldName;
+    private String fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("Resource %s not found field name : %s and field value : %s",
+                            resourceName, fieldName,fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
+}
