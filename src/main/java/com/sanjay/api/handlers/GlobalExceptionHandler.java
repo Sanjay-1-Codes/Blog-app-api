a081a596a49311ed571b08fa6e6f167e123bdaa2
Sanjay Kumar S <s.sanjaykumar1@zohomail.in>
2022-07-08 22:12:39 +0530

    "Added global exception handlers"

--- src/main/java/com/sanjay/api/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/sanjay/api/handlers/GlobalExceptionHandler.java
@@ -6,22 +6,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalClientArgumentException.class)
-    public ResponseEntity<ExceptionMessage> illegalClientArgumentException(IllegalClientArgumentException ex) {
-        return new ResponseEntity<>(new ExceptionMessage(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ExceptionMessage> illegalClientArgumentException(IllegalClientArgumentException ex, WebRequest webRequest) {
+        return new ResponseEntity<>(new ExceptionMessage(LocalDateTime.now(), ex.getMessage(), webRequest.getDescription(false)), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ExceptionMessage> resourceNotFoundException(ResourceNotFoundException ex) {
-        return new ResponseEntity<>(new ExceptionMessage(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ExceptionMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest webRequest) {
+        return new ResponseEntity<>(new ExceptionMessage(LocalDateTime.now(), ex.getMessage(), webRequest.getDescription(false)), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NumberFormatException.class)
-    public ResponseEntity<ExceptionMessage> numberFormatException(NumberFormatException ex) {
-        return new ResponseEntity<>(new ExceptionMessage(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ExceptionMessage> numberFormatException(NumberFormatException ex, WebRequest webRequest) {
+        return new ResponseEntity<>(new ExceptionMessage(LocalDateTime.now(), ex.getMessage(), webRequest.getDescription(false)), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionMessage> globalException(Exception ex, WebRequest webRequest) {
+        return new ResponseEntity<>(new ExceptionMessage(LocalDateTime.now(), ex.getMessage(), webRequest.getDescription(false)), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
